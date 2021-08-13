bf8c3cd0a0f1db795e4031a62c27c5864cf7a4c7
Dmytro <dmitry.tkk@gmail.com>
2021-08-13 13:21:34 +0300

    SKALE-4457 Update wait for contracts script

--- scripts/wait_for_contracts.sh
+++ scripts/wait_for_contracts.sh
@@ -5,7 +5,31 @@ set -e
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export IMA_SDK_DIR=$DIR/../skale-ima-sdk
 
-while [ ! -f $IMA_SDK_DIR/contracts_data/proxySchain_Bob.json ]; do echo "Waiting for contracts"; sleep 5; done
+COUNTER=0
+RETRIES_COUNT=20
+SLEEP_TIMEOUT=5
 
-cat $IMA_SDK_DIR/contracts_data/proxySchain_Bob.json
-echo 'DONE!'
+MAINNET_CONTRACTS_ABI_PATH=$IMA_SDK_DIR/contracts_data/proxyMainnet.json
+SCHAIN_CONTRACTS_ABI_PATH=$IMA_SDK_DIR/contracts_data/proxySchain_Bob.json
+
+while [ ! -f $MAINNET_CONTRACTS_ABI_PATH ]; do
+    echo "Waiting for mainnet contracts $COUNTER/$RETRIES_COUNT";
+    if [ $COUNTER -ge $RETRIES_COUNT ]; then
+        echo "Contracts wasn't deployed on time :("
+        exit 1;
+    fi
+    COUNTER=$[COUNTER + 1];
+    sleep $SLEEP_TIMEOUT;
+done
+echo "Mainnet contracts deployed: $MAINNET_CONTRACTS_ABI_PATH"
+
+while [ ! -f $SCHAIN_CONTRACTS_ABI_PATH ]; do
+    echo "Waiting for sChain contracts $COUNTER/$RETRIES_COUNT";
+    if [ $COUNTER -ge $RETRIES_COUNT ]; then
+        echo "Contracts wasn't deployed on time :("
+        exit 1;
+    fi
+    COUNTER=$[COUNTER + 1];
+    sleep $SLEEP_TIMEOUT;
+done
+echo "sChain contracts deployed: $SCHAIN_CONTRACTS_ABI_PATH"
