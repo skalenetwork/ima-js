848a30e6c7d2912930d03278821d8d948d5ea36b
Dmytro <dmitry.tkk@gmail.com>
2022-04-11 23:29:49 +0100

    SKALE-5020 Use ima-sdk from develop

--- scripts/deploy_erc721_with_metadata.sh
+++ scripts/deploy_erc721_with_metadata.sh
@@ -17,5 +17,9 @@ npx hardhat erc721meta --name $TOKEN_NAME --symbol $TOKEN_SYMBOL --network schai
 MAINNET_ERC_721_META_ADDRESS=$(cat $DIR/../test-tokens/data/ERC721MetaExample-$TOKEN_NAME-mainnet.json | jq -r ".erc721meta_address")
 SCHAIN_ERC_721_META_ADDRESS=$(cat $DIR/../test-tokens/data/ERC721MetaExample-$TOKEN_NAME-schain.json | jq -r ".erc721meta_address")
 
+echo Token manager addres: $TOKEN_MANAGER_ERC_721_META_ADDRESS
+echo Mainnet token: $MAINNET_ERC_721_META_ADDRESS
+echo sChain token: $SCHAIN_ERC_721_META_ADDRESS
+
 npx hardhat add-minter-erc721-meta --token-address $SCHAIN_ERC_721_META_ADDRESS --address $TOKEN_MANAGER_ERC_721_META_ADDRESS --network schain
 npx hardhat mint-erc721 --token-address $MAINNET_ERC_721_META_ADDRESS --receiver-address $TEST_ADDRESS --token-id $TEST_TOKEN_ID --network mainnet
